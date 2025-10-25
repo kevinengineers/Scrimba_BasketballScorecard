9cda4825eebc9e48edbd2c450142908871c3b428
Kevin Watke <kevinengineers@proton.me>
2025-10-25 15:48:34 -0700

    added buttons, created onclick functions that increase score when clicked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-let homeEl = document.getElementById('home-container')
-let guestEl = document.getElementById('guest-container')
-let homeScore = 0
-let guestScore = 0
+let homeEl = document.getElementById("home-container");
+let guestEl = document.getElementById("guest-container");
+let homeScore = 0;
+let guestScore = 0;
 
 function setupGame() {
-  homeEl.textContent = homeScore
-  guestEl.textContent = guestScore
+  homeEl.textContent = Number(homeScore);
+  guestEl.textContent = Number(guestScore);
 }
 
-setupGame()
+function addOne(state) {
+  if (state == "home") {
+    homeEl.textContent = Number(homeEl.textContent) + 1;
+  } else {
+    guestEl.textContent = Number(guestEl.textContent) + 1;
+  }
+}
+
+function addTwo(state) {
+  if (state == "home") {
+    homeEl.textContent = Number(homeEl.textContent) + 2;
+  } else {
+    guestEl.textContent = Number(guestEl.textContent) + 2;
+  }
+}
+
+function addThree(state) {
+  if (state == "home") {
+    homeEl.textContent = Number(homeEl.textContent) + 3;
+  } else {
+    guestEl.textContent =  Number(guestEl.textContent) + 3;
+  }
+}
+
+setupGame();
